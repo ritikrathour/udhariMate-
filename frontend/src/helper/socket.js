7c93e7bbf394782c1b0cff0f1099740cb0576d54
Ritik Rathour <ritikrathour73@gmail.com>
2025-04-17 00:13:23 +0530

    fix socket url

--- frontend/src/helper/socket.js
+++ frontend/src/helper/socket.js
@@ -1,5 +1,8 @@
 import { io } from "socket.io-client";
 const isProduction = process.env.NODE_ENV === 'production'; 
-const socket = io(isProduction ? import.meta.env.VITE_BASE_URL_PRODUCTION :"http://localhost:9000");
+const socket = io(isProduction ? import.meta.env.VITE_SOCKET_BASE_URL_PRODUCTION :"http://localhost:9000", {
+    transports: ["websocket"],
+    withCredentials: true
+});
 
 export default socket;
